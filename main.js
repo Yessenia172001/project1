5aa6ade72a535940e5ab34d581ef2ecce098b175
Yessenia172001 <alihanova.20016@gmail.com>
2023-09-05 18:28:50 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const a = 123,
+ b = 'abc';
+const c = true;
+
+const obj = {
+    name: 'Kolya',
+}
+
+const arr = ['name', 123];
+
+let y = 'i';
+let x = 'love';
+
+console.log(`${y} ${x}`);
+
+const ab = 'hello';
+const bc = 'world';
+
+console.log(`${ab} ${bc}`);
+
+
+let incr = 13;
+let decr = 12;
+
+// incr++; // постфиксный инкремент
+//++incr  префиксный инкремент
+
+console.log(++decr)
+
